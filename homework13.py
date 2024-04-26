2d1b8df339159318aa385092f06bf3ed761a4d55
TortevAL <165218904+TortevAL@users.noreply.github.com>
2024-04-26 20:00:33 +0300

    Update homework13.py

--- homework13.py
+++ homework13.py
@@ -1,8 +1,15 @@
-def test(txt, n, a: 6):
+def test_(a, b=2, c=3.5, txt='Job'):
+    print(a, b, c, txt)
+
+
+test_(15, 1, 15, 'BMV')
+
+
+def test(txt, n):
     if n < 0:
         return 0
     else:
-        return txt, n + (n - 1) ** a
+        return txt, n + (n - 1)
 
 
-print(test('Итог: ', 5, 6))
+print(test('Итог: ', 9))
